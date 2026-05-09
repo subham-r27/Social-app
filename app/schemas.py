c58df16f2c37c94c6252b1a8f51edaeba2ba497d
SUBHAM ROUT <subhamrout.max23@gmail.com>
2026-05-09 19:09:09 +0530

    Implemented vote/like feature

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel,EmailStr,ConfigDict
 from datetime import datetime
 from typing import Optional
 
+from pydantic.types import conint
+
 class PostBase(BaseModel):
     title: str
     content: str
@@ -43,3 +45,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
