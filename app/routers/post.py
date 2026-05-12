393dd2a8126f883a2bc17de68439f8cb63299599
SUBHAM ROUT <subhamrout.max23@gmail.com>
2026-05-13 02:02:53 +0530

    implemented for single post

--- app/routers/post.py
+++ app/routers/post.py
@@ -41,12 +41,15 @@ def create_posts(post:schemas.PostCreate,db:Session = Depends(get_db),current_us
     db.refresh(new_post)
     return new_post
 
-@router.get("/{id}",response_model=schemas.Post)
+@router.get("/{id}",response_model=schemas.PostOut)
 def get_post(id : int,response : Response,db:Session = Depends(get_db),current_user:int=Depends(oauth2.get_current_user)):
     #cursor.execute("""SELECT * from posts WHERE id=%s""", (str(id),))
     #post=cursor.fetchone()
 
-    post=db.query(models.Post).filter(models.Post.id == id).first()
+    #post=db.query(models.Post).filter(models.Post.id == id).first()
+
+    post=db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id,isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+
     if not post :
         #response.status_code=status.HTTP_404_NOT_FOUND
         raise HTTPException(
@@ -54,7 +57,7 @@ def get_post(id : int,response : Response,db:Session = Depends(get_db),current_u
             detail=f"post with id {id} was not found"
             )
 
-    if post.owner_id != current_user.id :
+    if post.Post.owner_id != current_user.id :
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested Action")
     
     return post
