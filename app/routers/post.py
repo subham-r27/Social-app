5030f456e7504c481f3f1c4fc2b3b4af2e4459c1
SUBHAM ROUT <subhamrout.max23@gmail.com>
2026-05-13 01:52:47 +0530

    Implemented Join functionality to return likes with posts data

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,7 @@
 from argparse import Action
 from fastapi import FastAPI,Response,HTTPException,status,Depends,APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from .. import models,schemas,oauth2
 from .. database import get_db
 from typing import List,Optional
@@ -13,13 +14,16 @@ router = APIRouter(
 )
 
 
-@router.get("/",response_model=List[schemas.Post])
+#@router.get("/",response_model=List[schemas.Post])
+@router.get("/",response_model=List[schemas.PostOut])
 def get_posts(db:Session = Depends(get_db),current_user:int=Depends(oauth2.get_current_user),limit: int=10,skip: int=0,search: Optional[str]=""):
     #cursor.execute("""SELECT * FROM posts""")
     #posts=cursor.fetchall()
     #%20 in url for space while searching
     
-    posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    #posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts=db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id,isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
--- app/schemas.py
+++ app/schemas.py
@@ -30,6 +30,13 @@ class Post(PostBase):
     class Config:
         orm_mode=True
 
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        from_attributes = True
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
