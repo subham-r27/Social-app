7007199c6d4a7728d320e297a30f40f7a60b1ace
SUBHAM ROUT <subhamrout.max23@gmail.com>
2026-05-14 19:30:24 +0530

    added CORSMiddleWare

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import user,post,auth,vote
@@ -7,6 +8,18 @@ from .config import settings
 models.Base.metadata.create_all(bind=engine) #not necessary for alembic
 
 app = FastAPI()
+
+origins=["https://www.google.com",
+"https://www.youtube.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
